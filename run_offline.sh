e011231c8c5335f40a9ccb22eaa6a03f17b37072
TeenSpirit1107 <123090540@link.cuhk.edu.cn>
2025-07-25 15:58:01 -0500

    [bash] include cuda blocking = 1

--- run_offline.sh
+++ run_offline.sh
@@ -3,6 +3,7 @@
 # export MODEL_PATH=/local/ymteng/models/Llama-3-8B-QServe
 # model 2: instruct
 export MODEL_PATH=/local/ymteng/models/Llama-3-8B-Instruct-QServe
+export CUDA_LAUNCH_BLOCKING=1
 
 # Set required environment variables for GPU blocks
 export NUM_RETRIEVAL_GPU_PAGE_BLOCKS=3200
